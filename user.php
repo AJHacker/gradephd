8ed44211298233e8fb8834191874c92655dc4df5
AJHacker <arihant@quixoa.com>
2017-05-17 14:37:10 -0400

    Update user.php

--- user.php
+++ user.php
@@ -4,7 +4,11 @@
 <?php
   $name = $_POST["name"];
   $pass = $_POST["password"];
-  
+  if(file_exists ( "/users/"+$name )){
+    echo 'USER FOUND';
+  } else {
+    echo 'USER CREATED'; 
+  }
 ?>
 
 
