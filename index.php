25e3a3cb8cfbaedc90693e2ed948a42c9c6f0f1f
AJHacker <arihant@quixoa.com>
2017-05-17 14:47:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
 <?php
   echo $_GET["error"];
 ?>
+  <h3>Login</h3>
   <form action="user.php" method="post">
   AndrewID: <input type="text" name="name"><br>
   Password: <input type="password" name="password"><br>
   <input type="submit">
-</form>
+  </form>
+  
 
 </body>
 </html>
