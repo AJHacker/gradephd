874974a261054540a849cbf0673555cf528bc13b
AJHacker <arihant@quixoa.com>
2017-05-18 15:56:08 -0400

    asdf

--- user.php
+++ user.php
@@ -6,11 +6,12 @@
 <body>
 <p>
 <?php
+
 	$action = $_POST["action"];
 	$email = $_POST["email"];
 	$pass = $_POST["password"];
 	$repass = $_POST["repass"];
-
+	echo $action . $email .$pass .$repass;
 	function pg_connection_string_from_database_url() {
 	    extract(parse_url($_ENV["DATABASE_URL"]));
 	    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
