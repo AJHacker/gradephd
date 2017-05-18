9f484f49597be68ab6335cfc3a8e9b98632ef46e
AJHacker <arihant@quixoa.com>
2017-05-18 11:11:15 -0400

    Add files via upload

---
+++ makedbs.php
@@ -0,0 +1,33 @@
+
+<?php
+
+	function pg_connection_string_from_database_url() {
+	    extract(parse_url($_ENV["DATABASE_URL"]));
+	    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+	}
+	# Here we establish the connection. Yes, that's all.
+	$db = pg_connect(pg_connection_string_from_database_url());
+	if(!$db)echo "ERROR";
+	if($db)echo "WORKS";
+	pg_close($db);
+	exit();
+
+	$sql =<<<EOF
+      CREATE TABLE USERS
+      (
+      EMAIL            TEXT    NOT NULL,
+      PASSWORD         TEXT    NOT NULL,
+      CLASS1           TEXT    NOT NULL,
+      CLASS2           TEXT    NOT NULL,
+      CLASS3           TEXT    NOT NULL,
+      CLASS4           TEXT    NOT NULL,
+      CLASS5           TEXT    NOT NULL
+      );
+EOF;
+
+
+	$result=pg_query($db,$sql);
+	echo pg_last_error();
+	echo $result;
+
+?>
--- user.php
+++ user.php
@@ -2,19 +2,14 @@
 <body>
 
 <?php
+  $action = $_POST["action"];
   $name = $_POST["name"];
   $pass = $_POST["password"];
-  if(file_exists ("/users/"+$name+strlen($pass))){
-    echo 'Classes:';
-  } else {
-    if (mkdir("/users/" + $name+strlen($pass), 0700,true)){
-       echo 'User Created With Name ' + $name + 'and password length of ' + strlen($pass);
-    
-    } else{
-      echo 'error!'; 
-    }
-   
-  }
+  $repass = $_POST["repass"];
+  
+  
+
+
 ?>
 
 
