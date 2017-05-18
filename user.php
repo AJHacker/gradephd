f64bb3946ceceba4543654f976f7ff74b74500ee
AJHacker <arihant@quixoa.com>
2017-05-18 16:00:53 -0400

    ads

--- user.php
+++ user.php
@@ -11,7 +11,6 @@
 	$email = $_POST["email"];
 	$pass = $_POST["pass"];
 	$repass = $_POST["repass"];
-	echo $action . $email .$pass .$repass;
 	function pg_connection_string_from_database_url() {
 	    extract(parse_url($_ENV["DATABASE_URL"]));
 	    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
