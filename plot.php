af59fedb653f4cd33143bef31c44dbde30b59a90
dzy10 <d.yang.1450@gmail.com>
2017-05-31 22:02:18 -0500

    Update plot.php

--- plot.php
+++ plot.php
@@ -16,6 +16,12 @@
  
 
     <?php
+        function pg_connection_string_from_database_url() {
+            extract(parse_url($_ENV["DATABASE_URL"]));
+            return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+        }
+        $db = pg_connect(pg_connection_string_from_database_url());
+    
         $class      = $_GET['class'];
         $user       = $_SESSION['verifiedUser'];
         $user_sql   = "SELECT * FROM $class WHERE name='$user';";
