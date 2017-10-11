54aa5644837c526eb69b7308cea4d49ab8c2abaa
AJHacker <arihant@quixoa.com>
2017-10-11 15:33:01 -0400

    Fix Styling of Login Page

--- login.php
+++ login.php
@@ -1,38 +1,54 @@
 <head>
 <link rel="stylesheet" href="styles/global.css">
 <script>
-function yesorno(){
-    if (document.getElementById('signin').checked) {
+function yesorno(a){
+    if (a==0) {
         document.getElementById('repass').style.visibility = 'hidden';
+        document.getElementById('repassLabel').style.visibility = 'hidden';
+        document.getElementById('passLabel').style.visibility = 'visible';
         document.getElementById('pass').style.visibility = 'visible';
-
     }
-    if (document.getElementById('signup').checked) {
+    if (a==1) {
         document.getElementById('repass').style.visibility = 'visible';
         document.getElementById('pass').style.visibility = 'visible';
+        document.getElementById('repassLabel').style.visibility = 'visible';
+        document.getElementById('passLabel').style.visibility = 'visible';
 
     }
-    if (document.getElementById('reset').checked) {
+    if (a==2) {
         document.getElementById('repass').style.visibility = 'hidden';
         document.getElementById('pass').style.visibility = 'hidden';
+        document.getElementById('repassLabel').style.visibility = 'hidden';
+        document.getElementById('passLabel').style.visibility = 'hidden';
 
     }
 
 }
 </script>
 </head>
-
 <div class="container">
   <form action="/user.php" method="post">
-
-    <input checked = "checked" id="signin" name="action" type="radio" value="signin" onclick="javascript:yesorno();" >Sign In</input><br>
-    <input id="signup" name="action" type="radio" value="signup" onclick="javascript:yesorno();" >Sign Up</input><br>
-    <input id="reset" name="action" type="radio" value="reset" onclick="javascript:yesorno();" >Reset</input><br>
-
-    <input class = "textinput" name = "email" id="email" placeholder="Email" type="text" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}"></input><br>
-    <input class = "textinput" name = "pass" id="pass" placeholder="Password" type="password" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}"></input></br>
-    <input class = "textinput" name = "repass" id="repass" placeholder="Repeat password" type="password" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}" style="visibility: hidden;"></input></br>
-    <input class = "button" type="submit" value="Hello Sir?">
+    <div class='topbar'>
+        <center>
+    <input type = 'button' class='button' id="signin" name="action" value="Sign In" onclick="javascript:yesorno(0);" ></input>
+    <input type = 'button' class='button' id="signup" name="action" value="Sign Up" onclick="javascript:yesorno(1);" ></input>
+    <input type = 'button' class='button' id="reset" name="action" value="Reset" onclick="javascript:yesorno(2);"></input>
+    </center>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div class="left">
+        Email: <br>
+        <label id='passLabel'>Password: </label><br>
+        <label id='repassLabel'>Repeat Password: </label><br>
+    </div>
+    <div class="right">
+        <input class = "textinput" name = "email" id="email" type="text" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}"></input><br>
+        <input class = "textinput" name = "pass" id="pass" type="password" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}"></input></br>
+        <input class = "textinput" name = "repass" id="repass" type="password" pattern="[a-zA-Z0-9!@#$%^*_|]{1,25}" style="visibility: hidden;"></input></br>
+    </div>
+    <center><input class = "button" type="submit" value="Hello Sir?" style="position: relative;"></center>
   </form>
 </div>
 
